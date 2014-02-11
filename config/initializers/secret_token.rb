675f91d336b8933cdf7e23f3bef528754b746a55
Joakim Runeberg <joakim.runeberg@gmail.com>
2014-02-11 16:02:59 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Helsingforsalliansen::Application.config.secret_key_base = 'a930f45f46832234877d5f9a0d1a4c5eebb99f106578a3aa8bde909e8da389a2d265578dcd43d27117a157f259e55fa153194e89f3710680f20b854fec74ae22'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Helsingforsalliansen::Application.config.session_store :cookie_store, key: '_helsingforsalliansen_session'
